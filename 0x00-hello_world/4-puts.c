472b118ee7a6e841cf2000ab5984e28fa8892225
linda <lindamakapela3@gmail.com>
2023-02-16 15:38:20 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - a programthat prints a line with a function puts
@@ -8,5 +7,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 } 
