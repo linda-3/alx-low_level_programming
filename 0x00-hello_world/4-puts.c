5be442f8a820a98615205e8c58d6fff60f0b4eb6
linda <lindamakapela3@gmail.com>
2023-02-16 14:06:09 -0800

    two tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - a programthat prints a line with a function puts
+ * Return: 0
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return(0);
+} 
