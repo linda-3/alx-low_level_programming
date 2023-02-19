8f3d592791d7a8dea875085a999b501931d78353
linda <lindamakapela3@gmail.com>
2023-02-18 18:35:36 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - a programthat prints a line with a function puts
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** 
+/**
  * main - a program that prints a line using printf
- * return : 0 (Success)
+ * return: 0 (Success)
  */
 
 int main(void)
