a0f5c15fccac73a78dafd7c57aea45a86cf8d9e1
linda <lindamakapela3@gmail.com>
2023-02-24 10:36:46 -0800

    Is upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
@@ -10,9 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
-
-
-
-
+int _putchar(char c)
+void print_number(int n);
+	
 #endif
